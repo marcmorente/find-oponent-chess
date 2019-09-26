b248a72f5c029153cd75772ca701a9abc3209463
Marc Morente <mmorente@aprop-si.com>
2019-09-26 19:07:41 +0200

    change const variables to protected

--- src/database/MysqlDatabaseConnection.php
+++ src/database/MysqlDatabaseConnection.php
@@ -3,24 +3,19 @@
 abstract class MysqlDatabaseConnection
 {
 
-    const USERNAME = 'root';
-    const PASSWORD = '';
-    const HOST = 'localhost';
-    const DB_NAME = 'chess';
-    
+    protected $username = 'root';
+    protected $password = '';
+    protected $host = 'localhost';
+    protected $db_name = 'chess';
+    protected $data_source_name;
     protected $database_handle;
 
     public function __construct()
     {
-        $host = self::HOST;
-        $username = self::USERNAME;
-        $password = self::PASSWORD;
-        $db_name = self::DB_NAME;
-
         try {
-            $data_source_name = "mysql:host={$host};dbname={$db_name};charset=utf8";
-            $this->database_handle = new \PDO($data_source_name, $username, $password);
-            /* Only debug mode
+            $this->data_source_name = "mysql:host={$this->host};dbname={$this->db_name};charset=utf8";
+            $this->database_handle = new \PDO($this->data_source_name, $this->username, $this->password);
+            /*
             $this->database_handle->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             $this->database_handle->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);*/
         } catch (\PDOException $e) {
