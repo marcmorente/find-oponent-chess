4a8b1ce0373242b924e8f447f7462ce45c438a14
Marc Morente <mmorente@inthinking.co.uk>
2020-09-23 07:50:23 +0200

    refactor class SplitName.php

--- src/includes/SplitName.php
+++ src/includes/SplitName.php
@@ -10,21 +10,18 @@ class SplitName
 
     public function buildSplit()
     {
-        $split   = preg_split('/[\s,]/', $this->search, null, PREG_SPLIT_NO_EMPTY);
         $w_query = [];
         $b_query = [];
         $binds   = [];
         $regex   = [];
-
+        $split   = preg_split('/[\s,]/', $this->search, null, PREG_SPLIT_NO_EMPTY);
+        
         foreach ($split as $key => $name) {
             $w_query[]               = 'white_player LIKE :'.$key.'wname';
-            $regex[]                 = "(?=.*$name)";
-            $binds[':'.$key.'wname'] = '%'.$name.'%';
-        }
-
-        foreach ($split as $key => $name) {
             $b_query[]               = 'black_player LIKE :'.$key.'bname';
             $binds[':'.$key.'bname'] = '%'.$name.'%';
+            $binds[':'.$key.'wname'] = '%'.$name.'%';
+            $regex[]                 = "(?=.*$name)";
         }
 
         $final_query = '('.implode(' AND ', $w_query). ') OR ('.implode(' AND ', $b_query).')';
