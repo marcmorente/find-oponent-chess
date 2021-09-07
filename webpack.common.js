9a23e023dfb6669f5a024fde3ea963f342ef5c74
Marc Morente <mmorente@inthinking.co.uk>
2021-09-07 15:59:20 +0200

    added webpack

--- webpack.common.js
+++ webpack.common.js
@@ -42,7 +42,7 @@ module.exports = {
         type: "asset",
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: "file-loader",
